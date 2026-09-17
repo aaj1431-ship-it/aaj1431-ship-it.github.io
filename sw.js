3aeefb866742b1fe50631b8616c9c49e11a4aabb
aaj1431-ship-it <aaj1431@gmail.com>
2026-09-17 16:26:29 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,21 @@
-// يخزّن ملفات التطبيق ليعمل بدون إنترنت (ما عدا التعرف على الصوت)
-const CACHE = 'tasbih-v1';
+// الشبكة أولًا: يحصل المستخدم دائمًا على آخر نسخة، ويعمل بدون إنترنت من النسخة المحفوظة
+const CACHE = 'tasbih-v2';
 const FILES = ['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
 ));
-self.addEventListener('fetch', e => e.respondWith(
-  caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-    const copy = res.clone();
-    caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
-    return res;
-  }).catch(() => r))
-));
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
